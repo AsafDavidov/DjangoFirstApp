d960c8712d93bf1fe5f2752a7fa9e4f5e91407f4
AsafDavidov <adavidov@conncoll.edu>
2019-04-26 18:04:41 -0400

    Have django return JSON objects. Index page

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from polls.models import Question
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    questions = Question.objects.all().values('question_text', 'pub_date')
+    questions_list = list(questions)
+    return JsonResponse(questions_list, safe=False)
 # Create your views here.
